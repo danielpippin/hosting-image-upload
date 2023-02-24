dd0d2ec89c25690d06efbd02fc3102d67240ab06
Daniel Pippin <danielpippin986@gmail.com>
2023-02-24 09:21:19 +0100

    scaffold article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "articles#index"
+  resources :articles, except: :index
+end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles, except: :index
 end
